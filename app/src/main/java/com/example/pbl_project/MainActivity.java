269824e8de807f991c87ceb40f70461e0d2e8906
hyerinsecu <heyrin8403@naver.com>
2020-07-01 22:45:51 +0900

    hyerin_commit

--- app/src/main/java/com/example/pbl_project/MainActivity.java
+++ app/src/main/java/com/example/pbl_project/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hihi
+        // It is harder than I thought :( ...
+        //hahahaha...
     }
 }
