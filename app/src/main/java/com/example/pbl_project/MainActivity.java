24cd69987c805cc9947b07672c3cc050ac20eacc
ajy0104 <anjeyun0104@naver.com>
2020-07-01 22:15:02 +0900

    add comment

--- app/src/main/java/com/example/pbl_project/MainActivity.java
+++ app/src/main/java/com/example/pbl_project/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //에러나지 않게 해주세요.
     }
 }
