ef56a13c56cfe11ea5a2de2774910d57bcd6fc9c
JDB <eksqlqj@naver.com>
2020-07-20 04:05:35 +0900

    FIRST TEST-JDB

---
+++ app/src/main/java/com/example/pbl_project/FirstFragment.java
@@ -0,0 +1,28 @@
+package com.example.pbl_project;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment(){}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.id.fragment_first, container, false);
+        return layout;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/pbl_project/pagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.pbl_project;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+public class pagerAdapter extends FragmentStatePagerAdapter {
+    ArrayList<Fragment> items = new ArrayList<Fragment>();
+
+    public pagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch(position)
+        {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    public void addItem(Fragment item){
+        items.add(item);
+    }
+}
