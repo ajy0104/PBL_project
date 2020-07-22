849d46decc9ed503c06af3d0ceceb7e16fd21dc9
JDB <eksqlqj@naver.com>
2020-07-23 03:36:43 +0900

    FIRST TEST-JDB(출근하기 UI)

--- app/src/main/java/com/example/pbl_project/AttendanceActivity.java
+++ app/src/main/java/com/example/pbl_project/AttendanceActivity.java
@@ -1,37 +1,19 @@
 package com.example.pbl_project;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+
 
 
 public class AttendanceActivity extends AppCompatActivity {
 
-    private TextView txtTitle; // 연/월 텍스트뷰
-    private Button btnPrev;
-    private Button btnNext;
-
-
-    //private GridAdapter gridAdapter; // 그리드뷰 어댑터
-    private ArrayList<String> dayList;     // 요일 저장 할 리스트
-    private GridView gridView;  // 그리드뷰
-    private Calendar mCal; // 캘린더변수
     private ViewPager vp;
 
 
@@ -51,9 +33,7 @@ public class AttendanceActivity extends AppCompatActivity {
         FragmentSecond fragment2 = new FragmentSecond();
         vpAdapter.addItem(fragment2);
 
-
         vp.setAdapter(vpAdapter);
-
 
     }
 
