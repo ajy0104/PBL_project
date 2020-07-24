9b3de7e0a06b803344c5fa5645c2d2e639eaf20f
JDB <eksqlqj@naver.com>
2020-07-25 01:35:10 +0900

    FIRST TEST-JDB(출근하기 UI)

--- app/src/main/java/com/example/pbl_project/FragmentFirst.java
+++ app/src/main/java/com/example/pbl_project/FragmentFirst.java
@@ -27,6 +27,8 @@ public class FragmentFirst extends Fragment {
     private ArrayList<String> dayList;     // 요일 저장 할 리스트
     private GridView gridView;  // 그리드뷰
     private Calendar mCal; // 캘린더변수
+
+    private TextView txtTitle;
     private Button btnLastMonth, btnNextMonth;
 
     public FragmentFirst(){}
@@ -43,8 +45,10 @@ public class FragmentFirst extends Fragment {
     {
         LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.fragment_first, container, false);
 
-        TextView txtTitle = (TextView)layout.findViewById(R.id.txtCalTitle);
+        txtTitle = (TextView)layout.findViewById(R.id.txtCalTitle);
         gridView = (GridView)layout.findViewById(R.id.gridview1);
+        btnLastMonth = layout.findViewById(R.id.btnLastMonth);
+        btnNextMonth = layout.findViewById(R.id.btnNextMonth);
 
         long now = System.currentTimeMillis();
         final Date date = new Date(now);
@@ -69,19 +73,68 @@ public class FragmentFirst extends Fragment {
 
         mCal = Calendar.getInstance();
 
+
         // 이번달 1일 무슨요일인지 판단 mCal.set(Year,Month,Day)
         mCal.set(Integer.parseInt(curYearFormat.format(date)), Integer.parseInt(curMonthFormat.format(date)) - 1, 1);
         int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
+
         //1일 - 요일 매칭 시키기 위해 공백 add
         for (int i = 1; i < dayNum; i++) {
             dayList.add("");
         }
-        setCalendarDate(mCal.get(Calendar.MONTH) + 1);
 
+        setCalendarDate(mCal.get(Calendar.MONTH) + 1);
         gridAdapter = new FragmentFirst.GridAdapter(getActivity(),dayList);
         //gridAdapter = new FragmentFirst.GridAdapter(getApplicationContext(), dayList);
         gridView.setAdapter(gridAdapter);
 
+        btnLastMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCal = getLastMonth(mCal);
+
+                dayList = new ArrayList<String>();
+                dayList.add("일");
+                dayList.add("월");
+                dayList.add("화");
+                dayList.add("수");
+                dayList.add("목");
+                dayList.add("금");
+                dayList.add("토");
+                int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
+
+                //1일 - 요일 매칭 시키기 위해 공백 add
+                for (int i = 1; i < dayNum; i++) {
+                    dayList.add("");
+                }
+
+                setCalendarDate(mCal.get(Calendar.MONTH) + 1);
+            }
+        });
+
+        btnNextMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCal = getNextMonth(mCal);
+
+                dayList = new ArrayList<String>();
+                dayList.add("일");
+                dayList.add("월");
+                dayList.add("화");
+                dayList.add("수");
+                dayList.add("목");
+                dayList.add("금");
+                dayList.add("토");
+                int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
+
+                //1일 - 요일 매칭 시키기 위해 공백 add
+                for (int i = 1; i < dayNum; i++) {
+                    dayList.add("");
+                }
+
+                setCalendarDate(mCal.get(Calendar.MONTH) + 1);
+            }
+        });
 
         return layout;
 
@@ -94,13 +147,17 @@ public class FragmentFirst extends Fragment {
      * @param month
      */
     private void setCalendarDate(int month) {
+
         mCal.set(Calendar.MONTH, month - 1);
 
         for (int i = 0; i < mCal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
             dayList.add("" + (i + 1));
         }
-
+        gridAdapter = new FragmentFirst.GridAdapter(getActivity(),dayList);
+        //gridAdapter = new FragmentFirst.GridAdapter(getApplicationContext(), dayList);
+        gridView.setAdapter(gridAdapter);
     }
+
 
     // 그리드뷰 어댑터
     private class GridAdapter extends BaseAdapter {
@@ -155,6 +212,7 @@ public class FragmentFirst extends Fragment {
             mCal = Calendar.getInstance();
             //오늘 day 가져옴
             Integer today = mCal.get(Calendar.DAY_OF_MONTH);
+
             String sToday = String.valueOf(today);
             if (sToday.equals(getItem(position))) { //오늘 day 텍스트 컬러 변경
                 holder.tvItemGridView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
@@ -166,5 +224,37 @@ public class FragmentFirst extends Fragment {
     private class ViewHolder {
         TextView tvItemGridView;
     }
+
+    /**
+     * 지난달의 Calendar 객체를 반환합니다.
+     *
+     * @param calendar
+     * @return LastMonthCalendar
+     */
+    private Calendar getLastMonth(Calendar calendar)
+    {
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), 1);
+        calendar.add(Calendar.MONTH, -1);
+        txtTitle.setText(mCal.get(Calendar.YEAR) + "년 "
+                + (mCal.get(Calendar.MONTH) + 1) + "월");
+        return calendar;
+    }
+
+    /**
+     * 다음달의 Calendar 객체를 반환합니다.
+     *
+     * @param calendar
+     * @return NextMonthCalendar
+     */
+    private Calendar getNextMonth(Calendar calendar)
+    {
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), 1);
+        calendar.add(Calendar.MONTH, +1);
+        txtTitle.setText(mCal.get(Calendar.YEAR) + "년 "
+                + (mCal.get(Calendar.MONTH) + 1) + "월");
+        return calendar;
+    }
+
+
 
 } // FragmentFirst
